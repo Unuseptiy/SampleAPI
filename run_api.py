a9bb618d9f386a514134f4a179a2226da7bc1845
Unuseptiy <unuseptiy@fake.com>
2021-07-05 21:40:53 +0300

    Change to ORM

--- run_api.py
+++ run_api.py
@@ -3,10 +3,12 @@ from hashlib import sha256
 
 import tornado.ioloop
 import tornado.web
-from sqlalchemy import create_engine, select, Table, Column, Integer, String, MetaData, DateTime, update
-from datetime import datetime
+from sqlalchemy import create_engine, select, update
+import os
 
 import yaml
+from sqlalchemy.orm import Session
+from db_init import CollectedData, Users
 
 
 class BaseHandler(tornado.web.RequestHandler, ABC):
@@ -19,30 +21,25 @@ class AuthHandler(BaseHandler, ABC):
     """Handler for authentication"""
     def post(self):
         self.set_header("Content-Type", "application/json")
-        input_ = self.get_argument("pass")
-        sha_entered_pass = sha256(input_.encode('utf-8')).hexdigest()
-        url = "postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}".format(
-            DB_USERNAME=d["DB_USERNAME"], DB_PASSWORD=d["DB_PASSWORD"], DB_NAME=d["DB_NAME"])
-        engine = create_engine(url)
-        conn = engine.connect()
+        input_pass = self.get_argument("pass")
+        sha_entered_pass = sha256(input_pass.encode('utf-8')).hexdigest()
+        input_login = self.get_argument("login")
+
         # finding login in database
-        s = select([users]).where(users.c.login == self.get_argument("login"))
-        r = conn.execute(s)
-        row = r.fetchone()
-        if not row:
+        person = session.query(Users).filter(Users.login == input_login).first()
+        if not person:
             # if this username does not exist – an error
             raise tornado.web.HTTPError(404)
         else:
-            u = update(users).where(users.c.login == self.get_argument("login"))
-            conn.execute(u)
-            if sha_entered_pass == row[2]:
+            session.query(Users).filter(Users.login == input_login).update({"login": input_login}, synchronize_session='fetch')
+            session.commit()
+            if sha_entered_pass == person.password:
                 if not self.get_secure_cookie("mycookie"):
                     # setting a cookie if the password is correct and the cookie is not set
                     self.set_secure_cookie("mycookie", self.get_argument("login"))
             else:
                 # if this password is incorrect – an error
                 raise tornado.web.HTTPError(404)
-        conn.close()
 
 
 class GetDataHandler(BaseHandler, ABC):
@@ -51,15 +48,11 @@ class GetDataHandler(BaseHandler, ABC):
         if not self.current_user:
             # If the user is not authenticated, an error occurs
             raise tornado.web.HTTPError(401)
-        url = "postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}".format(
-            DB_USERNAME=d["DB_USERNAME"], DB_PASSWORD=d["DB_PASSWORD"], DB_NAME=d["DB_NAME"])
-        engine = create_engine(url)
-        conn = engine.connect()
-        s = select([collected_data])
-        r = conn.execute(s)
+
+        data_about_countries = session.query(CollectedData).all()
 
         # names of the table fields
-        titles: list = ['id', 'COUNTRY', 'COUNTRY CODE', 'ISO CODES', 'POPULATION', 'AREA KM', 'GDP $USD']
+        titles = ['id', 'country', 'country_code', 'iso_codes', 'population', 'area', 'gdp']
 
         # dictionary for inference the information
         out_dict: dict = {"data": [],
@@ -67,47 +60,29 @@ class GetDataHandler(BaseHandler, ABC):
                           }
 
         # counter of the number of records in collected_data
-        total: int = 0
+        total = len(data_about_countries)
 
         # out_dict dictionary filling for response
-        for row in r:
-            total += 1
-            title_pointer: int = 0
+        for row in data_about_countries:
             row_dict: dict = {}
-            for item in row:
-                row_dict[titles[title_pointer]] = item
-                title_pointer += 1
+            for title in titles:
+                row_dict[title] = getattr(row, title)
             out_dict["data"].append(row_dict)
         out_dict["total"] = total
-        conn.close()
         self.set_header("Content-type", "application/json")
         self.write(out_dict)
 
 
 if __name__ == "__main__":
-    metadata = MetaData()
-    # creating collected_data table
-    collected_data = Table('collected_data', metadata,
-                           Column('id', Integer(), primary_key=True, nullable=False),
-                           Column('country', String(100), nullable=False),
-                           Column('country_code', String(50), nullable=False),
-                           Column('iso_codes', String(50), nullable=False),
-                           Column('population', String(200)),
-                           Column('area', String(200), nullable=False),
-                           Column('gdp', String(50)),
-                           )
-
-    # creating users table
-    users = Table('users', metadata,
-                  Column('id', Integer(), primary_key=True, nullable=False),
-                  Column('login', String(100), unique=True, nullable=False),
-                  Column('password', String(100), nullable=False),
-                  Column('created_at', DateTime(), default=datetime.utcnow(), nullable=False),
-                  Column('last_request', DateTime(), onupdate=datetime.utcnow())
-                  )
-
-    with open(r"/Users/elenakozenko/Desktop/task_job/config.yaml", "r") as file:
+    path = os.getcwd()
+    with open(os.path.join(path, "../config.yaml"), "r") as file:
         d = yaml.safe_load(file)
+
+    url = "postgresql+psycopg2://{DB_USERNAME}:{DB_PASSWORD}@localhost/{DB_NAME}".format(DB_USERNAME=d["DB_USERNAME"],
+                                                                                         DB_PASSWORD=d["DB_PASSWORD"],
+                                                                                         DB_NAME=d["DB_NAME"])
+    engine = create_engine(url)
+    session = Session(bind=engine)
 
     application = tornado.web.Application([
         (r"/api/login", AuthHandler),
